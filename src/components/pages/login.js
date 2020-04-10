8820a2d363907ad47e858e8540acc6753c9c2f62
NAD <mtv.duyna@gmail.com>
2020-04-10 16:43:22 +0700

    c

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -4,6 +4,7 @@ import {Logo} from '../atoms';
 import {SignInForm} from '../templates';
 import {useNavigation} from '@react-navigation/native';
 import AsyncStorage from '@react-native-community/async-storage';
+import axios from '../../utilities/axios';
 
 const Login = props => {
   const navigation = useNavigation();
@@ -11,7 +12,21 @@ const Login = props => {
   const getData = async () => {
     const value = await AsyncStorage.getItem('@token');
     if (value !== null) {
-      navigation.navigate('Main');
+      axios
+        .post('me')
+        .then(res => {
+          if (res.status === 200) {
+            navigation.navigate('Main');
+          }
+        })
+        .catch(err =>
+          configs.alert.alertWithType(
+            'error',
+            'Đăng nhập tự động thất bại',
+            err,
+          ),
+        );
+      // navigation.navigate('Main');
     }
   };
 
